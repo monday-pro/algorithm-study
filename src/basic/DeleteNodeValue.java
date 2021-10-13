97c9b87ca221d0f8e4bdc744ed9d4d7655bf2caa
ff-monday <1024469732@qq.com>
2021-10-13 14:21:38 +0800

    Initial commit

---
+++ src/basic/DeleteNodeValue.java
@@ -0,0 +1,41 @@
+package basic;
+
+/**
+ * 单链表删除指定的值
+ *
+ * @author Java和算法学习--周一
+ */
+public class DeleteNodeValue {
+
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static Node removeValue(Node head, int num) {
+        while (head != null) {
+            if (head.value != num) {
+                break;
+            }
+            head = head.next;
+        }
+
+        Node pre = head;
+        Node cur = head;
+        while (cur != null) {
+            if (cur.value == num) {
+                pre.next = cur.next;
+            } else {
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+
+        return head;
+    }
+
+}
---
+++ src/basic/ReverseNode.java
@@ -0,0 +1,62 @@
+package basic;
+
+/**
+ * 单链表和双链表反转
+ *
+ * @author Java和算法学习--周一
+ */
+public class ReverseNode {
+
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public static class DoubleNode {
+        public int value;
+        public DoubleNode last;
+        public DoubleNode next;
+
+        public DoubleNode(int value) {
+            this.value = value;
+        }
+    }
+
+    /**
+     * a    ->   b    ->  c  ->  null
+     * c    ->   b    ->  a  ->  null
+     *
+     * @return 返回头节点
+     */
+    public static Node reverseNode(Node head) {
+        Node pre = null;
+        Node next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+    public static DoubleNode reverseDoubleNode(DoubleNode head) {
+        DoubleNode pre = null;
+        DoubleNode next = null;
+
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            head.last = next;
+            pre = head;
+            head = next;
+        }
+
+        return pre;
+    }
+
+}
